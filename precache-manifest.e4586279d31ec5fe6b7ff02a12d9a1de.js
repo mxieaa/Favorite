9145672bdd42f4bc43b3a027f2344c3d285000fc
mxieaa <46963517+mxieaa@users.noreply.github.com>
2022-03-22 10:00:03 +0800

    Add files via upload

---
+++ precache-manifest.e4586279d31ec5fe6b7ff02a12d9a1de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "57d1889edc98c9aec2d9",
+    "url": "./static/js/main.57d1889e.chunk.js"
+  },
+  {
+    "revision": "343df934504d3090a7e5",
+    "url": "./static/js/1.343df934.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4a289cfe37fba196b7cb0a2a54270afd",
+    "url": "./static/media/logo-black.4a289cfe.png"
+  },
+  {
+    "revision": "ebfa846f60f745cfed9ddf73242348fb",
+    "url": "./index.html"
+  }
+];
